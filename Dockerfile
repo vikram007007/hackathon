f26f22058f147c0a9797486b734fc82c6892d9fa
vikram_007 <alexandervikram80@gmail.com>
2020-10-14 13:02:35 +0000

    docker

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,8 @@
+FROM maven:3.6.0-jdk-11-slim AS build
+COPY src /usr/src/app/src
+COPY pom.xml /usr/src/app
+RUN mvn -f /usr/src/app/pom.xml clean package
+
 FROM openjdk:11.0
 COPY --from=build /usr/src/app/target/SpringBootApplication-0.0.1-SNAPSHOT.jar /usr/app/app.jar
 WORKDIR /usr/app
--- src/main/java/com/example/hackathon/controller/StockController.java
+++ src/main/java/com/example/hackathon/controller/StockController.java
@@ -8,6 +8,7 @@ import yahoofinance.Stock;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,6 +30,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/stock")
+@CrossOrigin
 public class StockController {
 	
 	@Autowired
